ff5c535ffb2956638d709fe79382b45958c911c1
Rodrigo Matos Lourenço <r.matos96@gmail.com>
2025-04-03 12:41:08 -0300

    department

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department objDepartment = new Department(1, "books");
+		
+		System.out.println(objDepartment);
+	}
+
+}
